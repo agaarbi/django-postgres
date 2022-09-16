6c6e515f6c172d77b0d034733e41a2d92959d90e
ghani <agaarbi@gmail.com>
2022-09-16 11:54:08 +0500

    password converted to hash using bcrypt in models

--- postgres/accounts/models.py
+++ postgres/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+import bcrypt
 
 # Create your models here.
 
@@ -10,5 +11,12 @@ class Accounts(models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     username = models.CharField(max_length=120)
-    password = models.CharField(max_length=120)
+    password = models.CharField(max_length=300)
     email = models.EmailField(max_length=254)
+
+    def save(self, *args, **kwargs):
+        password = str(self.password)
+        password = password.encode('utf-8')
+        self.password = bcrypt.hashpw(password, bcrypt.gensalt())
+        # print(self.password)
+        super(Accounts, self).save(*args, **kwargs)
